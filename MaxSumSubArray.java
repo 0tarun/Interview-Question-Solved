4a9bf86d8c2190be5927a521e0f515e0c630af10
0tarun <tdas221498@bscse.uiu.ac.bd>
2024-07-02 22:24:09 +0600

    MaxSum

---
+++ MaxSumSubArray.java
@@ -0,0 +1,22 @@
+public class MaxSumSubArray {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println("MaxSumSubArray  :" + MaxSum(arr) + "");
+
+    }
+
+    public static int MaxSum(int[] arr) {
+        int max = arr[0];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
